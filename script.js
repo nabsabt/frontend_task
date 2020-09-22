33bc41816bc175bdcf0560310400a945001f4d66
Bence Nagy <nabsabt@gmail.com>
2020-09-22 11:13:50 +0200

    Code cleaned a bit, comments added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+/*
+--Function initialization--
+Each function below triggers a defined DIV's display method
+*/
 
 function managementOn(){
+  /*
+  -The management_div's, as DOM model's address is given to an Object,
+   as value.
+  -Via this object, we toggle the element's visibility to "inline".
+  -This pattern goes to the other 2 Div's as well
+  */
  
 var managementDivObj = document.getElementById("management_div");
 managementDivObj.style.display = "inline";
@@ -8,6 +18,12 @@ managementDivObj.style.display = "inline";
 
 function managementOff(){
 
+  /*
+  -The management_div's, as DOM model's address is given to an Object,
+   as value.
+  -Via this object, we toggle the element's visibility to "none".
+  -This pattern goes to the other 2 Div's as well
+  */
   var managementDivObj = document.getElementById("management_div");
   managementDivObj.style.display = "none";
   
@@ -32,10 +48,15 @@ function allocationOff(){
   var AllocationDivObj = document.getElementById("allocation_eng_spec");
   AllocationDivObj.style.display = "none";
 }
-
+/*
+-EventListener is added, which is being called only if the DOM model
+ is built already.
+*/
 
 window.load = (event) => {
- 
+ /*
+  -The already declared functions are being called here.
+  */
   managementOn();
   managementOff();
   FTPEngineOn();
